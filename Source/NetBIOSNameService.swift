36d4b2183e6606fcd0fb35d7e953b7ea03601fde
Seth Faxon <seth.faxon@gmail.com>
2017-08-30 17:55:44 -0700

    basic setup of repo

---
+++ Source/NetBIOSNameService.swift
@@ -0,0 +1,16 @@
+//
+//  NetBIOSNameService.swift
+//  SMBClient
+//
+//  Created by Seth Faxon on 8/30/17.
+//  Copyright © 2017 Filmic. All rights reserved.
+//
+
+import Foundation
+
+import "netbios_ns.h"
+
+public struct NetBIOSNameService {
+    
+    private nameService =
+}
