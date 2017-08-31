a3315218c9161fa3ee1b1a218b53264948ed2e43
Seth Faxon <seth.faxon@gmail.com>
2017-08-30 17:45:43 -0700

    Initial Commit

---
+++ SMBClient/SMBClient.h
@@ -0,0 +1,19 @@
+//
+//  SMBClient.h
+//  SMBClient
+//
+//  Created by Seth Faxon on 8/30/17.
+//  Copyright © 2017 Filmic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMBClient.
+FOUNDATION_EXPORT double SMBClientVersionNumber;
+
+//! Project version string for SMBClient.
+FOUNDATION_EXPORT const unsigned char SMBClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMBClient/PublicHeader.h>
+
+
---
+++ SMBClientTests/SMBClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  SMBClientTests.swift
+//  SMBClientTests
+//
+//  Created by Seth Faxon on 8/30/17.
+//  Copyright © 2017 Filmic. All rights reserved.
+//
+
+import XCTest
+@testable import SMBClient
+
+class SMBClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
